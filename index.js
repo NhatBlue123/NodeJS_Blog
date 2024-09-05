023589707a38f44545dcef2a96ce158bd1b09dac
Nguyen Thanh Nhat <nguyennhat082004@gmail.com>
2024-09-05 15:44:05 +0700

    install morgan

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express')
+var morgan = require('morgan')
+
 const app = express()
 const port = 3000
+
+app.use(morgan('combined'))
+
 
 app.get('/home', (req, res) => {
   res.send('Hello World!')
